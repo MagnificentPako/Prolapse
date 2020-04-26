32570bc555f8e61ea4a97eac10d46a5cc5f65b9c
MagnificentPako <my@procrastination.life>
2020-04-26 12:54:05 +0200

    experimenting with quasiqoting

--- util.pl
+++ util.pl
@@ -1,10 +1,10 @@
+:- quasi_quotation_syntax(message).
+
 
 words(S, W) :- split_string(S, " ", "", W).
 unwords(W, S) :-
     reverse(W, R),
     foldl(string_concat, R, "", S).
-
-
 
 cb(S, Lang) --> "```", Lang, "\n", S, "```".
 cb(S) --> cb(S, "").
@@ -14,3 +14,22 @@ codeblock(Str, Res) :-
 codeblock(Str, Lang, Res) :-
     phrase(cb(Str, Lang), As),
     string_codes(Res, As).
+
+/*
+:- set_prolog_flag(double_quotes, chars).
+
+message_syntax(_, _, []) -->
+    "".
+
+message_syntax(Vars, _Dict, [mention(user, Id)]) -->
+    '@',
+    number(Nth),
+    !,
+    { nth0(Nth, Vars, Id) }.
+
+message_syntax(Vars, Dict, [E|Elem]) -->
+   [E], message_syntax(Vars, Dict, Elem).
+
+message(Content, Vars, Dict, Mention) :-
+    phrase_from_quasi_quotation(message_syntax(Vars, Dict, Mention), Content).
+*/
