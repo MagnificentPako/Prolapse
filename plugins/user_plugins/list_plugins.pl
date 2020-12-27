44744ae8a5133747b78486a4c05f1a667c34cc8a
Sævar Berg <saevar.berg.saevarsson@itelligence.dk>
2020-12-27 17:38:00 +0100

    Make ::help invoke list_plugins

--- plugins/user_plugins/list_plugins.pl
+++ plugins/user_plugins/list_plugins.pl
@@ -1,6 +1,7 @@
 :- multifile user_plugin/2.
 
 user_plugin("list_plugins", list_plugins).
+user_plugin("help", list_plugins).
 list_plugins(_, Msg) :-
     findall(user_plugin(P, H), user_plugin(P, H), Ps),
     with_output_to(string(Pretty), listing(user_plugin)),
